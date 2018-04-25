dc06322eea06eee232a83f4870bf2e3f8c44d230
Tiago Correa <tiagopoa@gmail.com>
2018-04-24 22:55:59 -0300

    change date quase terminado

--- calculaData.py
+++ calculaData.py
@@ -1,54 +1,125 @@
 #!/usr/bin/python
 
+print()
 date = str(input('Entre com a data (dd/MM/yyyy HH24:mi): '))
+print()
 op = str(input('Entre com a operação desejada (+ ou -): '))
+print()
 value = int(input('Entre com a quantidade de minutos: '))
-
-days_jan = 31
-days_fev = 28
-days_mar = 31
-days_apr = 30
-days_may = 31
-days_jun = 30
-days_jul = 31
-days_aug = 31
-days_sep = 30
-days_oct = 31
-days_nov = 30
-days_dec = 31
+print()
 
 def change_date(date,op,value):
-    #Aqui eu separo a data em partes, dia, mes, ano, horas e minutos
+    #Aqui eu separo a data em partes, dia, mes, ano, horas e minutos.
     day=int(date[0:2])
     month=int(date[3:5])
     year=int(date[6:10])
     hour=int(date[11:13])
     minute=int(date[14:16])
 
-    #Aqui eu descubro quantos anos, meses, dias, horas e minutos precisam ser incrementados ou decrementados
+    #Aqui eu descubro quantos dias, horas e minutos precisam ser incrementados ou decrementados.
     years = int(value/365/1440)
     months = int(value/30/1440)
     days = int(value/1440)
     hours = int(value%1440/60)
     minutes = int(value%60)
-    #Aqui exibe a quantidade de tempo que precisa ser incrementada ou decrementada
+
+    #Aqui exibe a quantidade de tempo que precisa ser incrementada ou decrementada.
+    print('Esta é a quantidade de dias, horas e minutos que será somada ou subtraida da data informada inicialmente, dependendo da opção que você escolheu')
     print (days,'Dias',hours,'Horas',minutes,'Minutos')
 
+    #Aqui eu defino qual a quantidade máxima de dias que o mês inicial possui.
+    #Em seguida vou controlando a quantidade de dias a ser decrementada pela variavel days_left.
+    #Vou somando os dias na variavel day e verificando se o mês tem 28, 30 ou 31 dias.
+    #Quando a variavel day alcança o limite da variavel max_month_days é incrementado em 1 a variavel month.
+    #Mas se a variavel month já tiver o valor 12, então a variável month recebe o valor 1 e então incrementa a variavel year em 1.
+    max_month_days=month
     days_left=days
     while days_left>0:
-        if day<30:
-            day+=1
-            days_left-=1
+        if month==1:
+            max_month_days_before=31
+            max_month_days=31
+        elif month==2:
+            max_month_days_before=31
+            max_month_days=28
+        elif month==3:
+            max_month_days_before=28
+            max_month_days=31
+        elif month==4:
+            max_month_days_before=31
+            max_month_days=30
+        elif month==5:
+            max_month_days_before=30
+            max_month_days=31
+        elif month==6:
+            max_month_days_before=31
+            max_month_days=30
+        elif month==7:
+            max_month_days_before=30
+            max_month_days=31
+        elif month==8:
+            max_month_days_before=31
+            max_month_days=31
+        elif month==9:
+            max_month_days_before=31
+            max_month_days=30
+        elif month==10:
+            max_month_days_before=30
+            max_month_days=31
+        elif month==11:
+            max_month_days_before=31
+            max_month_days=30
+        elif month==12:
+            max_month_days_before=30
+            max_month_days=31
         else:
-            day=1
-            days_left-=1
-            month+=1
+            print('erro')
+            break
 
-    print('mes',month)
+        if op==str('+'):
+            if day<max_month_days:
+                day+=1
+                days_left-=1
+            else:
+                day=1
+                days_left-=1
+                if month<12:
+                    month+=1
+                else:
+                    month=1
+                    year+=1
+            #Neste ponto ajusta-se as horas e minutos.
+            if (hour+hours)>24 and days_left==0:
+                hour=(hour+hours)%24
+                day+=1
 
+#Aqui inicia o calculo para decrementar dias, horas e minutos.
+        if op==str('-'):
+            if day<=max_month_days and day>1:
+                day-=1
+                days_left-=1
+            else:
+                day=max_month_days_before              
+                days_left-=1
+                if month>1:
+                    month-=1
+                else:
+                    month=12
+                    year-=1
 
+    #Neste ponto ajusta-se as horas e minutos.
+            if (hour-hours)>0 and days_left==0:
+                hour=(hour-hours)
+                if minutes>0:
+                    hour-=1
+                    minutes=60-minutes
 
-    print('Data atualizada:',day,'/',month,'/',year,hour,':',minute)
+    print()
+    #Aqui é exibido o resultado da data inicial + ou - a quantidade em minutos, dependendo da opção escolhida (+ ou -).
+    #Mostrando qual data será após a quantidade de minutos informada.
+    print('Data atualizada:',day,'/',month,'/',year,hour,':',minutes)
+    print()
+
+    return('Data atualizada:',day,'/',month,'/',year,hour,':',minutes)    
 
 change_date(date,op,value)   
 
@@ -56,44 +127,4 @@ change_date(date,op,value)
 
 
 
-    #while minutes>0:
-    #    minute+=minutes  
-    #    minutes=0        
-    #    if hour < 23:
-    #        hour+=1
-    #    else:
-    #        hour=0
-    #        day+=1
-    #     
-    #    #print('resultado do minute',minute)
-    #
-    #    while hours>0:
-    #        hour+=hours
-    #        if hour>=23:
-    #            hour=hour%24              
-    #            hours=0
-    #            if day<30:
-    #                day+=1
-    #            else:
-    #                day=1
-    #                month+=1
-    #    #print('resultado do hours',hour)
-
-        #while days>0:
-        #    day+=days
-        #    print('resultado day+=days',day)
-        #    if day>=30:
-        #        day=day%30
-        #        month+=1
-        #        days=0
-        #days=0
-                
-        #print('resultado do days',day)
-
-        #while months>0:
-        #    month+=months
-        #    if month>=12:
-        #        month=months%12
-        #        months=0
-        #        year+=1
-        #print('resultado do months',month)
+    
