1eda31501ffce9c3b70d4d95b664866930e6bfbc
Tiago Correa <tiagopoa@gmail.com>
2018-04-25 15:17:09 -0300

    finalizado

--- calculaData.py
+++ calculaData.py
@@ -15,27 +15,27 @@ value = int(input('Entre com a quantidade de minutos: '))
 print()
 
 def change_date(date,op,value):
-#Aqui eu separo a data em partes, dia, mes, ano, horas e minutos.
+#Separa a data em partes, dia, mes, ano, horas e minutos.
     day=int(date[0:2])
     month=int(date[3:5])
     year=int(date[6:10])
     hour=int(date[11:13])
     minute=int(date[14:16])
 
-#Aqui eu descubro quantos dias, horas e minutos precisam ser incrementados ou decrementados.
+#Descobre quantos dias, horas e minutos precisam ser incrementados ou decrementados.
     years = int(value/365/1440)
     months = int(value/30/1440)
     days = int(value/1440)
-    hours = int(value%1440/60)
+    hours = int((value%1440)/60)
     minutes = int(value%60)
 
-#Aqui exibe a quantidade de tempo que precisa ser incrementada ou decrementada.
+#Exibe a quantidade de tempo que precisa ser incrementada ou decrementada.
     print('Esta é a quantidade de dias, horas e minutos que será somada ou subtraida da data informada inicialmente, dependendo da opção que você escolheu')
     print (days,'Dias',hours,'Horas',minutes,'Minutos')
 
-#Aqui eu defino qual a quantidade máxima de dias que o mês inicial possui.
-#Em seguida vou controlando a quantidade de dias a ser decrementada pela variavel days_left.
-#Vou somando os dias na variavel day e verificando se o mês tem 28, 30 ou 31 dias.
+#Quantidade máxima de dias que o mês inicial possui.
+#Controla a quantidade de dias a ser decrementada pela variavel days_left.
+#Soma os dias na variavel day e verificando se o mês tem 28, 30 ou 31 dias.
 #Quando a variavel day alcança o limite da variavel max_month_days é incrementado em 1 a variavel month.
 #Mas se a variavel month já possuir o valor 12, então a variável month recebe o valor 1 e então incrementa a variavel year em 1.
     max_month_days=month
@@ -81,8 +81,8 @@ def change_date(date,op,value):
         else:
             print('erro')
             
-        if op==str('+'):
-            if day<max_month_days:
+        if op=='+':
+            if day<max_month_days and days_left>0:
                 day+=1
                 days_left-=1
             else:
@@ -93,13 +93,14 @@ def change_date(date,op,value):
                 else:
                     month=1
                     year+=1
-#Neste ponto ajusta-se as horas e minutos.
+
+#Austa as horas e minutos.
             if (hour+hours)>24 and days_left==0:
                 hour=(hour+hours)%24
                 day+=1
 
-#Aqui inicia o calculo para decrementar dias, horas e minutos.
-        if op==str('-'):
+#Calculo para decrementar dias, horas e minutos.
+        if op=='-':
             if day<=max_month_days and day>1:
                 day-=1
                 days_left-=1
@@ -112,7 +113,7 @@ def change_date(date,op,value):
                     month=12
                     year-=1
 
-#Neste ponto ajusta-se as horas e minutos.
+#Ajusta as horas e minutos.
             if (hour-hours)>0 and days_left==0:
                 hour=(hour-hours)
                 if minutes>0:
@@ -120,12 +121,11 @@ def change_date(date,op,value):
                     minutes=60-minutes
 
     print()
-#Aqui é exibido o resultado da data inicial + ou - a quantidade em minutos, dependendo da opção escolhida (+ ou -).
-#Mostrando qual data será após a quantidade de minutos informada.
+#Exibe o resultado da data inicial + ou - a quantidade em minutos, dependendo da opção escolhida (+ ou -).
     print('Data atualizada:',day,'/',month,'/',year,hour,':',minutes)
     print()
 
-    return('Data atualizada:',day,'/',month,'/',year,hour,':',minutes)    
+    return(day,month,year,hour,minutes)    
 
 change_date(date,op,value)   
 
